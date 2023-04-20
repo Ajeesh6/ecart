4a73c5ace086e3a81aafa5176de7a1a95406fb00
Ajeesh V S <ajeeshvs185@gmail.com>
2023-04-20 21:52:50 +0530

    initial

---
+++ src/app/products/product.service.ts
@@ -0,0 +1,40 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+// import behaviour subject
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  // object create for behaviour subject
+  search=new BehaviorSubject("")
+
+  constructor(private http:HttpClient) { }
+
+  //Api call to view all products
+  viewAllProduct(){
+   return this.http.get('http://localhost:3000/products')
+  }
+
+  //api call to view single product
+  viewProduct(productId:any){
+    return this.http.get('http://localhost:3000/products/'+productId)
+  }
+
+  //api to delete product
+  DeleteProductComponent(productId:any){
+    return this.http.delete('http://localhost:3000/products/'+productId)
+  }
+
+  //api call to add new product
+  addNewProduct(productData:any){
+    return this.http.post('http://localhost:3000/products',productData)
+  }
+
+  //api call to update product
+  updateProduct(productId:any,productData:any){
+    return this.http.put('http://localhost:3000/products/'+productId,productData)
+  }
+}
